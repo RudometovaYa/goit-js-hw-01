6840195b696afb19011d88e02a4bdc81511a643b
Yana Rudometova <rudometova.ya@gmail.com>
2025-03-08 21:33:55 +0200

    1st hw

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+function getElementWidth (content, padding, border) {
+    const contentPx = Number.parseFloat(content);
+    const paddingPx = Number.parseFloat(padding);
+    const borderPx = Number.parseFloat(border);
+    const borderBox = (contentPx + (paddingPx * 2) + (borderPx * 2));
+
+    return borderBox;
+}
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px")); 
+console.log(getElementWidth("200px", "0px", "0px"));
